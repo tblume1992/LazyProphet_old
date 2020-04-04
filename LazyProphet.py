3fc2dd40429e3baf2af46d6acae5d9e449d1d5f1
tblume1992 <t-blume@hotmail.com>
2020-04-04 16:06:16 -0400

    Update LazyProphet.py

--- LazyProphet.py
+++ LazyProphet.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import numpy as np
-import scipy
+from scipy import signal
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import Ridge
-
-
 
 class LazyProphet():
   def __init__(self, 
                freq = 0, 
                estimator = 'linear', 
-               max_boosting_rounds = 50, 
+               max_boosting_rounds = 100, 
                l2 = 1, 
                poly = 3, 
                nested_seasonality = False, 
                ols_constant = False,
-               seasonal_smoothing = None,
+               seasonal_smoothing = 10,
                approximate_splits = False,
-               regularization = 1.2):
+               regularization = 1.2,
+               seasonal_esti = 'harmonic'):
     
     self.l2 = l2
     self.nested_seasonality = nested_seasonality
@@ -29,6 +28,7 @@ class LazyProphet():
     self.seasonal_smoothing = seasonal_smoothing
     self.approximate_splits = approximate_splits
     self.regularization = regularization
+    self.seasonal_esti = seasonal_esti
     
   def ridge(self,y):
     if len(y) == 1:
@@ -59,7 +59,7 @@ class LazyProphet():
       
     return predicted
   
-  def ols(self, y, bias, ols_constant = False):
+  def ols(self, y, bias, ols_constant):
     y = np.array(y - bias).reshape(-1, 1)
     X = np.array(range(len(y))).reshape(-1, 1)
     if ols_constant:
@@ -76,7 +76,7 @@ class LazyProphet():
       proposals = self.get_split_proposals()
       for index, i in enumerate(proposals): 
         predicted1 = self.ols(y[:i], 0, ols_constant = True)
-        predicted2 = self.ols(y[i:], predicted1[-1], ols_constant = False)
+        predicted2 = self.ols(y[i:], predicted1[-1], ols_constant = self.ols_constant)
         iteration_mae = np.mean(np.abs(y - np.append(predicted1,predicted2)))
         if index == 0:
           mae = iteration_mae
@@ -94,7 +94,13 @@ class LazyProphet():
     proposals = [i for i in proposals if i < len(self.boosted_data) - 10]
     
     return proposals
+
+  def get_fourier_series(self, t, p=365, n=10):
+    x = 2 * np.pi * np.arange(1, n + 1) / p
+    x = x * t[:, None]
+    self.fourier_series = np.concatenate((np.cos(x), np.sin(x)), axis=1)
     
+    return    
     
   def get_split_proposals(self):
     if self.approximate_splits:
@@ -103,32 +109,27 @@ class LazyProphet():
       return list(range(10, len(self.boosted_data) - 10))
   
   
-  def get_seasonality(self,y,freq):
+  def get_harmonic_seasonality(self,y,freq):
+    if not self.freq:
+      seasonality = np.zeros(len(self.time_series))
+    else:
+      X = self.fourier_series
+      X = np.append(X, np.asarray(np.ones(len(y))).reshape(len(y), 1), axis = 1)
+      beta =  np.linalg.pinv(X.T.dot(X)).dot(X.T.dot(y))
+      seasonality = X @ beta
+      
+    return seasonality
+
+  def get_naive_seasonality(self,y,freq):
     if not self.freq:
       seasonality = np.zeros(len(self.time_series))
     else:
       seasonality = np.array([np.mean(y[i::freq], axis=0) for i in range(freq)])
       if freq > 30:
-        b, a = scipy.signal.butter(20, 0.125)
-        seasonality = scipy.signal.filtfilt(b,a,seasonality)
+        b, a = signal.butter(20, 0.125)
+        seasonality = signal.filtfilt(b,a,seasonality)
       
     return seasonality
-  
-  def filtered_seasonality(self,total_seasonality):
-    if not self.freq:
-      filtered_sig = np.zeros(len(self.time_series))
-    else:
-      total_seasonality = np.append(total_seasonality[:self.freq],total_seasonality[0])
-      N = len(total_seasonality)
-      ft = np.fft.fft(total_seasonality - np.mean(total_seasonality))/N
-      ft[ft.argsort()[self.seasonal_smoothing:]] = 0
-      #welch = scipy.signal.welch(y) 
-      filtered_sig = (scipy.fftpack.ifft(ft+ np.mean(total_seasonality)))*N 
-      filtered_sig += np.mean(total_seasonality)
-      filtered_sig = filtered_sig[1:].real
-      filtered_sig = np.resize(np.array(filtered_sig)[:self.freq + 2], len(self.time_series))
-    
-    return filtered_sig
   
   def calc_cost(self, prediction, c):
     n = len(self.time_series)
@@ -139,6 +140,8 @@ class LazyProphet():
   def fit(self, time_series):
     self.time_series_index = time_series.index
     self.time_series = time_series.values
+    if self.seasonal_esti == 'harmonic':
+      self.get_fourier_series(np.arange(len(self.time_series)), self.freq, self.seasonal_smoothing)
     trends = []
     seasonalities = []
     nested_seasonalities = []
@@ -158,12 +161,15 @@ class LazyProphet():
         else:
           trend = self.linear(self.boosted_data).reshape(self.boosted_data.size,)
       resid = self.boosted_data-trend
-      seasonality = self.get_seasonality(resid, self.freq)
+      if self.seasonal_esti == 'harmonic':
+        seasonality = self.get_harmonic_seasonality(resid, self.freq)
+      elif self.seasonal_esti == 'naive':
+        seasonality = self.get_naive_seasonality(resid, self.freq)
       if self.freq:
         seasonality = np.tile(seasonality,
                               int(len(self.time_series)/self.freq + 1))[:len(self.time_series)]
       if self.nested_seasonality:
-        nested_seasonal_factor = self.get_seasonality(self.boosted_data-(trend + seasonality), 7)
+        nested_seasonal_factor = self.get_naive_seasonality(self.boosted_data-(trend + seasonality), 7)
         nested_seasonal_factor =  np.tile(nested_seasonal_factor,
                                           int(len(self.time_series)/7 + 1))[:len(self.time_series)]
         nested_seasonalities.append(nested_seasonal_factor)
@@ -183,11 +189,8 @@ class LazyProphet():
         cost = self.calc_cost(total_trend + total_seasonalities, i)
       else:
         break
-    if self.seasonal_smoothing:
-      self.total_seasonalities = self.filtered_seasonality(total_seasonalities)
-    else:
-      self.total_seasonalities = total_seasonalities
     self.total_trend = total_trend
+    self.total_seasonalities = total_seasonalities
     if self.nested_seasonality:
       total_nested_seasonality = np.sum(nested_seasonalities, axis = 0)
     output = {}
