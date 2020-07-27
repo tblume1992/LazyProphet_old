c9496cf0919da5b4a2d6351752d5145f716499ae
tblume1992 <t-blume@hotmail.com>
2020-07-26 22:50:10 -0400

    Add exogenous variable stuff and trend dampening

--- LazyProphet.py
+++ LazyProphet.py
@@ -3,22 +3,31 @@ import numpy as np
 from scipy import signal
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import Ridge
+import statsmodels.api as sm
+
 
 class LazyProphet:
   def __init__(self, 
                freq = 0, 
                estimator = 'linear', 
                max_boosting_rounds = 100, 
-               l2 = 1, 
+               l2 = .01, 
                poly = 3, 
                nested_seasonality = False, 
                ols_constant = False,
                seasonal_smoothing = 10,
-               approximate_splits = False,
+               approximate_splits = True,
                regularization = 1.2,
                seasonal_esti = 'harmonic',
-               split_cost = 'mae',
-               global_cost = 'mbic'):
+               split_cost = 'mse',
+               global_cost = 'maicc',
+               trend_dampening = 0,
+               seasonal_regularization = 'auto',   
+               exogenous = None,
+               verbose = 1,
+               n_split_proposals = 100,
+               min_samples = .15
+               ):
     
     self.l2 = l2
     self.nested_seasonality = nested_seasonality
@@ -33,6 +42,18 @@ class LazyProphet:
     self.seasonal_esti = seasonal_esti
     self.split_cost = split_cost
     self.global_cost = global_cost
+    self.trend_dampening = trend_dampening
+    self.exogenous = exogenous
+    self.verbose = verbose
+    self.n_split_proposals = n_split_proposals
+    self.min_samples = min_samples
+    if type(self.freq) != int:
+        raise Exception('Frequency must be an int')
+    
+    if seasonal_regularization != 'auto':
+        self.seasonal_regularization = 1 - seasonal_regularization 
+    else:
+        self.seasonal_regularization = seasonal_regularization
     self.coefs = np.ones((1,poly))
     
   def ridge(self,y):
@@ -102,10 +123,11 @@ class LazyProphet:
 
   def get_approximate_split_proposals(self):
     gradient = pd.Series(np.abs(np.gradient(self.boosted_data, edge_order=1)))
-    gradient = gradient.sort_values(ascending = False)[:100]
+    gradient = gradient.sort_values(ascending = False)[:self.n_split_proposals]
     proposals = list(gradient.index)
-    proposals = [i for i in proposals if i > 10]
-    proposals = [i for i in proposals if i < len(self.boosted_data) - 10]
+    min_split_idx = int(len(self.boosted_data)*.1)
+    proposals = [i for i in proposals if i > min_split_idx]
+    proposals = [i for i in proposals if i < len(self.boosted_data) - min_split_idx]
     
     return proposals
 
@@ -120,7 +142,8 @@ class LazyProphet:
     if self.approximate_splits:
       return self.get_approximate_split_proposals()
     else:
-      return list(range(10, len(self.boosted_data) - 10))
+      min_idx =int(max(5, len(self.time_series) * self.min_samples))
+      return list(range(min_idx, len(self.boosted_data) - min_idx))
   
   
   def get_harmonic_seasonality(self,y,freq):
@@ -131,8 +154,17 @@ class LazyProphet:
       X = np.append(X, np.asarray(np.ones(len(y))).reshape(len(y), 1), axis = 1)
       beta =  np.linalg.pinv(X.T.dot(X)).dot(X.T.dot(y))
       seasonality = X @ beta
-      
-    return seasonality
+      if self.seasonal_regularization == 'auto':
+          seasonal_reg = seasonality.reshape((len(seasonality), 1))
+          regularization = float(np.linalg.pinv(seasonal_reg.T.dot(seasonal_reg)).dot(seasonal_reg.T.dot(self.boosted_data)))
+          if regularization > 1:
+              regularization = 1
+          if regularization < 0:
+              regularization = 0
+          seasonality = seasonality * regularization
+      else:
+          seasonality = seasonality * self.seasonal_regularization
+    return seasonality 
 
   def get_naive_seasonality(self,y,freq):
     if not self.freq:
@@ -143,7 +175,7 @@ class LazyProphet:
         b, a = signal.butter(20, 0.125)
         seasonality = signal.filtfilt(b,a,seasonality)
       
-    return seasonality
+    return seasonality * self.seasonal_regularization
   
   def calc_cost(self, prediction, c):
     n = len(self.time_series)
@@ -166,18 +198,20 @@ class LazyProphet:
     seasonalities = []
     nested_seasonalities = []
     errors =[]
+    exo_predicted = []
+    exo_impact = []
     self.boosted_data = self.time_series
     for i in range(self.max_boosting_rounds):
       if self.estimator == 'ridge':
         trend = self.ridge(self.boosted_data).reshape(self.boosted_data.size,)
       elif self.estimator == 'mean':
         if i == 0:
-          trend = np.tile(np.mean(self.boosted_data), len(self.boosted_data))
+          trend = np.tile(np.median(self.boosted_data), len(self.boosted_data))
         else:
           trend = self.mean(self.boosted_data).reshape(self.boosted_data.size,)
       elif self.estimator == 'linear':
         if i == 0:
-          trend = np.tile(np.mean(self.boosted_data), len(self.boosted_data))
+          trend = self.ols(self.boosted_data, 0, True).reshape(self.boosted_data.size,)
         else:
           trend = self.linear(self.boosted_data).reshape(self.boosted_data.size,)
       resid = self.boosted_data-trend
@@ -195,12 +229,20 @@ class LazyProphet:
         nested_seasonalities.append(nested_seasonal_factor)
         self.boosted_data = self.boosted_data-(trend+seasonality+nested_seasonal_factor)
       else:
-        self.boosted_data = self.boosted_data-(trend+seasonality)      
+        self.boosted_data = self.boosted_data-(trend+seasonality)  
+      if self.exogenous is not None:
+          ols_model = sm.OLS(self.boosted_data, self.exogenous).fit()
+          #print(ols_model.summary())
+          exo_predict = ols_model.predict(self.exogenous)
+          self.boosted_data = self.boosted_data - exo_predict
+          exo_impact.append((ols_model.params, ols_model.cov_params()))
+          exo_predicted.append(exo_predict)
       errors.append(np.mean(np.abs(self.boosted_data)))
       trends.append(trend)
       seasonalities.append(seasonality)
       total_trend = np.sum(trends, axis = 0)
       total_seasonalities = np.sum(seasonalities, axis = 0)
+      total_exo = np.sum(exo_predicted, axis = 0)
       round_cost = self.calc_cost(total_trend + total_seasonalities, i)
       if i == 0:
         cost = round_cost
@@ -208,6 +250,7 @@ class LazyProphet:
         cost = round_cost
         self.total_trend = total_trend
         self.total_seasonalities = total_seasonalities
+        self.total_exo = total_exo
       else:
         break
 
@@ -216,24 +259,68 @@ class LazyProphet:
     output = {}
     output['y'] = self.time_series
     if self.nested_seasonality:
-      output['yhat'] = pd.Series(self.total_trend + 
+      yhat = pd.Series(self.total_trend + 
                                 self.total_seasonalities + 
-                                total_nested_seasonality, 
+                                total_nested_seasonality + self.total_exo, 
                                 index = self.time_series_index).astype(float)
     else:
-      output['yhat'] = pd.Series(self.total_trend + 
-                                self.total_seasonalities, 
+      yhat = pd.Series(self.total_trend + 
+                                self.total_seasonalities + self.total_exo, 
                                 index = self.time_series_index).astype(float)
-    output['trend'] = pd.Series(self.total_trend, index = self.time_series_index)
-    output['seasonality'] = pd.Series(self.total_seasonalities, 
+    trend = pd.Series(self.total_trend, index = self.time_series_index)
+    seasonality = pd.Series(self.total_seasonalities, 
                                       index = self.time_series_index)
+    exogenous = pd.Series(self.total_exo, index = self.time_series_index)
     if self.nested_seasonality:
       output['nested_seasonality'] = pd.Series(total_nested_seasonality, 
                                                 index = self.time_series_index)
-      
+    self.seasonal_strength = self.calc_seasonal_strength(self.time_series - yhat,
+                                      self.time_series - trend)
+    if self.seasonal_strength > 0 and self.seasonal_strength <= .15 and self.verbose:
+        print('Seasonal Signal is weak, try a different frequency or disable seasonality with freq=0')
+    self.trend_strength = self.calc_trend_strength(self.time_series - yhat,
+                                      self.time_series - seasonality)
+    
+    
+    upper_prediction, lower_prediction = self.get_prediction_intervals(self.time_series,
+                                                                       yhat)
+    if self.exogenous is not None:
+        output['Exogenous Prediction'] = exogenous
+        output['Exogenous Summary'] = self.get_boosted_exo_results(exo_impact)
+        self.exo_impact = exo_impact
+    if self.freq != 0:
+        c = i + self.seasonal_smoothing + 1
+    else:
+        c = i + 1
+    if self.estimator == 'ridge':
+        c += self.poly
+    n = len(self.time_series)
+    
+    self.model_cost = (2*c**2 + 2*c)/(n-c-1) + 2*(c**self.regularization) + \
+              n*np.log(np.sum((self.time_series - yhat )**2)/n) 
+    output['yhat'] = yhat
+    output['yhat_upper'] = upper_prediction
+    output['yhat_lower'] = lower_prediction
+    output['seasonality'] = seasonality
+    output['trend'] = trend
+    
     return output
 
-  def extrapolate(self, n_steps = 10):
+  def get_prediction_intervals(self, y, predicted):
+    sd_error = np.std(y - predicted)
+    t_stat = stats.t.ppf(.9, len(y))
+    upper = predicted + t_stat*sd_error
+    lower = predicted - t_stat*sd_error
+    
+    return upper, lower
+
+  def calc_seasonal_strength(self, resids, detrended):
+    return max(0, 1-(np.var(resids)/np.var(detrended)))
+
+  def calc_trend_strength(self, resids, deseasonalized):
+    return max(0, 1-(np.var(resids)/np.var(deseasonalized)))
+
+  def extrapolate(self, n_steps = 10, future_X = None):
     if self.estimator == 'mean':
       extrapolated_trend = np.tile(self.total_trend[-1], n_steps)
     else:
@@ -242,6 +329,53 @@ class LazyProphet:
                             self.total_trend[-1]
     extrapolated_seasonality = np.resize(self.total_seasonalities[:self.freq], 
                                          len(self.total_seasonalities) + n_steps)[-n_steps:]
+    
+    if self.trend_dampening:
+        extrapolated_trend = self.trend_dampen(self.trend_dampening, extrapolated_trend)
     extrapolated_series = extrapolated_trend + extrapolated_seasonality
+    if future_X is not None:
+        extrapolated_series += np.dot(future_X,np.array(self.beta).reshape(-1,1)).reshape(-1,)
     
     return extrapolated_series
+
+  def trend_dampen(self, damp_fact, trend):
+     zeroed_trend = trend - trend[0]
+     damp_fact = 1 - damp_fact
+     if damp_fact < 0:
+         damp_fact = 0
+     if damp_fact > 1:
+         damp_fact = 1
+     if damp_fact == 1:
+         dampened_trend = zeroed_trend
+     else:       
+         tau = (damp_fact*1.15+(1.15*damp_fact/.85)**9)*\
+                 (2*len(zeroed_trend))
+         dampened_trend = (zeroed_trend*np.exp(-pd.Series(range(1, len(zeroed_trend) + 1))/(tau)))
+         crossing = np.where(np.diff(np.sign(np.gradient(dampened_trend))))[0]
+         if crossing.size > 0:
+             crossing_point = crossing[0]
+             dampened_trend[crossing_point:] = dampened_trend[(np.mean(np.gradient(zeroed_trend))*dampened_trend).idxmax()]
+   
+     return dampened_trend + trend[0]
+ 
+  def get_boosted_exo_results(self, exo_impacts):
+    y = self.time_series
+    for i, element in enumerate(exo_impacts[:-1]):
+        if i == 0:
+            var = np.diag(element[1])
+            beta = element[0]
+        else:
+            var = ((len(y) - 1) / len(y))**2 * var +(1/len(y)**2)*(len(y) - 1)*np.diag(element[1])
+            beta += element[0]
+    se = np.sqrt(var)
+    t_stat = beta/se
+    pval = stats.t.sf(np.abs(t_stat), len(y)-1)*2
+    self.var = var
+    self.beta = beta
+    summary = {}
+    summary['P-Value'] = pval
+    summary['t-Stat'] = t_stat
+    summary['Coefficient'] = beta
+    summary['Standard Error'] = se
+    
+    return summary
